db97fe346d8ce7e767e4fbd55e01b2b316fd915f
Maximilian Tauber <maximilian.tauber@icloud.com>
2024-06-15 14:26:12 +0200

    add functions to load tasks from database in backlog.component

--- src/app/backlog/backlog.component.ts
+++ src/app/backlog/backlog.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Router } from '@angular/router';
+import { Task } from '../models/task';
 
 @Component({
   selector: 'app-backlog',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BacklogComponent implements OnInit {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth, public firestore: AngularFirestore, private router: Router) { }
+
+  allTasks: Task[] = [];
+  task = new Task();
+  taskTitle = this.task.title;
+  userUID: string = "";
+  allSortedTasks: Task[] = [];
+
 
   ngOnInit(): void {
+    this.afAuth.onAuthStateChanged(user => {
+      if (user) {
+        // User is signed in.
+        console.log('User is signed in:', user);
+        console.log(user.uid)
+        this.userUID = user.uid;
+      } else {
+        // No user is signed in.
+        console.log('No user is signed in.');
+      }
+    });
+    this.loadAllTasks();
+  }
+
+  loadAllTasks() {
+    console.log("loading all tasks")
+    this.firestore
+      .collection('tasks')
+      .valueChanges()
+      .subscribe((changes: any) => {
+        this.allTasks = changes;
+        console.log(this.allTasks);
+        console.log("All tasks loaded")
+        //this.sortTasks();
+      })
   }
 
   allSampleTasks = [
